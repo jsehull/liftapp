6688cc5732d2efb693774b4cd6c71895c1605843
Jesse Hull <jse.hull@gmail.com>
2016-05-03 14:07:09 -0400

    v0.2 nearing basic structure

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
 (function() {
 	var app = angular.module('liftApp', []);
 
+	app.controller('DayRoutine', funciton() {
 
+	});
 
+	var exerciseList = [
+		{
+			day: 'Deadlifts',
+			name: 'Calf Press',
+		},
+		{
+			day: 'Deadlifts',
+			name: 'Calf Press'
+		},
+		{
+			day: 'Deadlifts',
+			name: 'Calf Press'
+		},
+		{
+			day: 'Deadlifts',
+			name: 'Calf Press'
+		}
+	];
+
+	var exerciseList = {
+		Deadlifts: [
+			'Calf Press',
+			'Other Excercise'
+		],
+		Arms: [
+			'Calf Press',
+			'Other Excercise'
+		]
+	}
+
+	// where to store appended set data
 
 
 })();
