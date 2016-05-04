d8024eea6c9091b7573918dd9b589769df4b124f
Jesse Hull <jse.hull@gmail.com>
2016-05-04 17:09:53 -0400

    select inputs (dropdowns) working

--- app.js
+++ app.js
@@ -1,46 +1,80 @@
 (function() {
 	angular
 		.module('liftApp', ['ngAnimate', 'ui.bootstrap'])
-		.controller('RoutineController', function() {
-			var days = Object.keys(exercises);
-			var exercisesForDay = exercises[day];
-			// ng-click, 
+		.controller('DropdownController', function() {
+			this.routine = menuData;
 		});
 
+	var menuData = {
+		arms: {
+			name: 'Arms',
+			exerciseType: [
+				'Sit Down Curls',
+				'Single Overheads',
+	      'Standing Curl Dumbells',
+	      'Kickbacks',
+	      'Hammer Curls',
+	      'Skull Crushers',
+	      'Rope Down',
+	      'Standing Bar Curls'
+			]
+		},
+    deadlifts: {
+    	name: 'Deadlifts',
+    	exerciseType: [
+				'Calf Press',
+				'Deadlifts',
+	      'Forearm Front',
+	      'Forearm Back'
+			]
+		},
+    chest: {
+    	name: 'Chest',
+    	exerciseType: [
+				'Flat Bench',
+	      'Mid Press',
+	      'Decline Press',
+	      'Incline Bench',
+	      'Flys'
+			]
+		},
+    back: {
+    	name: 'Back',
+    	exerciseType: [
+				'Wide Rows',
+				'Behind Head Lat Pull',
+	      'Close Rows',
+	      'Frontside Lat Pull',
+	      'Rear Delt Rope Pull'
+			]
+		},
+    legs: {
+    	name: 'Legs',
+    	exerciseType: [
+				'Seated Calf Raises',
+				'Squats',
+	      'Front Squats',
+	      'Leg Curls',
+	      'Leg Extensions'
+			]
+		},
+    shoulders: {
+    	name: 'Shoulders',
+    	exerciseType: [
+				'Single Arm Raises',
+	      'Bent Over Raises',
+	      'Front Arm Raises (bar)',
+	      'Dumbell Press',
+	      'Shrugs',
+	      'Chin Raises'
+			]
+		}
+	}
 
-	// var exerciseList = [
-	// 	{
-	// 		day: 'Deadlifts',
-	// 		name: 'Calf Press',
-	// 	},
-	// 	{
-	// 		day: 'Deadlifts',
-	// 		name: 'Calf Press'
-	// 	},
-	// 	{
-	// 		day: 'Deadlifts',
-	// 		name: 'Calf Press'
-	// 	},
-	// 	{
-	// 		day: 'Deadlifts',
-	// 		name: 'Calf Press'
-	// 	}
-	// ];
 
-	var exercises = {
-		Deadlifts: [
-			'Calf Press',
-			'deadlift'
-		],
-		Arms: [
-			'curl',
-			'standing curl'
-		]
-	};
 
 
 	// where to store appended set data
 
 
 })();
-
